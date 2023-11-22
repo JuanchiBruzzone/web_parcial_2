50efe5bfccfa3a73ad9eaa6ef0fc7808daa1c966
Juan Ignacio Bruzzone <juanignacio@akesse.com.uy>
2023-11-22 00:32:52 -0300

    edited readme and swagger code gen

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# web_parcial_2
+# Parcial 2 - Desarrollo Web y Mobile
+
+## Instrucciones:
+
+1) npm install
+2) npm run dev
+3) navegar a http://localhost:3000/api-docs/
+
+### Notas: 
+#### NO USAR EL PARAMETRO AUTHORIZATION EN LOS ENDPOINTS.
+#### PRIMERO USAR EL ENDPOINT [GET] /auth/token y luego con ese token continuar las requests siguientes.
--- src/swagger.ts
+++ src/swagger.ts
@@ -3,8 +3,10 @@ import swaggerAutogen from 'swagger-autogen';
 const doc = {
     info: {
         version: 'v1.0.0',
-        title: 'Swagger Demo Project',
-        description: 'Implementation of Swagger with TypeScript'
+        title: 'Parcial 2 - Desarrollo Web y Mobile',
+        description: 'Implementacion de Swagger para Express/Node. NO USAR EL PARAMETER AUTORIZATION EN LOS ENDPOINTS.' +
+            'PRIMERO USAR /auth/token y con el resultado dar click en Authorize y colocar el token ahi, luego se pueden hacer' +
+            'todas las requests que se desean.'
     },
     servers: [
         {
